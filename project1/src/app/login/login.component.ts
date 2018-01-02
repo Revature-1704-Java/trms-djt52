dc4244a29451194bde1599ebe90c3206e9a8f91e
Daniel <djt52@case.edu>
2018-01-01 22:17:41 -0500

    Front end work connecting to servlets

--- project1/src/app/login/login.component.ts
+++ project1/src/app/login/login.component.ts
@@ -8,14 +8,20 @@
 export class LoginComponent implements OnInit {
 
   public attemptLogin() {
+  
       let email = document.getElementById("loginform").elements[0].value;
       let pass = document.getElementById("loginform").elements[1].value;
       console.log(email);
       console.log(pass);
       let xml = new XMLHttpRequest();
-      xml.open('GET',`http://localhost:8080/login?email=${email}&pass=${pass}`,true);
+      xml.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+       }
+    };
+      xml.open('GET',`http://localhost:8080/Reimbursement/LoginServlet?email=${email}&pass=${pass}`,true);
       xml.send();
-
+    
   }
   
 
--- project1/src/app/yourforms/yourforms.component.ts
+++ project1/src/app/yourforms/yourforms.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-yourforms',
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./yourforms.component.css']
 })
 export class YourformsComponent implements OnInit {
-
-  constructor() { }
-
+  
   ngOnInit() {
-  }
+    let i = 1:number;
+    let xml = new XMLHttpRequest();
+        xml.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+              processTable(this);
+         }
+      };
+      xml.open('GET',`http://localhost:8080/Reimbursement/YourServlet?eid=${i}`,true);
+      xml.send();
+   }
 
-}
+   function processTable(xml) {
+      let i = 1:number;
+      let xmlDoc = this.responseXML;
+      let table=`<tr><th>ID</th><th>Status</th><th>Description</th><th>Cost</th><th>Reimbursement Amount</th>
+      <th>Location</th><th>Date</th><th>Time</th><th>Reason</th><th>Format</th><th>Event</th></tr>`;
+      let x = xmlDoc.getElementsByTagName("CD");
+      for (i = 0; i <x.length; i++) { 
+        table += "<tr><td>" +
+        x[i].getElementsByTagName("ID")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("STATUS")[0].childNodes[0].nodeValue +
+        "</td></tr>";
+        x[i].getElementsByTagName("DESCRIPTION")[0].childNodes[0].nodeValue +
+        "</td></tr>";
+        x[i].getElementsByTagName("COST")[0].childNodes[0].nodeValue +
+        "</td></tr>";
+        x[i].getElementsByTagName("RAMOUNT")[0].childNodes[0].nodeValue +
+        "</td></tr>";
+        x[i].getElementsByTagName("LOCATION")[0].childNodes[0].nodeValue +
+        "</td></tr>";
+        x[i].getElementsByTagName("DATE")[0].childNodes[0].nodeValue +
+        "</td></tr>";
+        x[i].getElementsByTagName("TIME")[0].childNodes[0].nodeValue +
+        "</td></tr>";
+        x[i].getElementsByTagName("REASON")[0].childNodes[0].nodeValue +
+        "</td></tr>";
+        x[i].getElementsByTagName("FORMATID")[0].childNodes[0].nodeValue +
+        "</td></tr>";
+        x[i].getElementsByTagName("EVENTID")[0].childNodes[0].nodeValue +
+        "</td></tr>";
+         }
+      document.getElementById("demo").innerHTML = table;
+      }
+      }
+
